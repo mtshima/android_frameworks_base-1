17b89b314e09e77779830dd593699aae24186cae
tobitege <tobiasteschner@googlemail.com>
2015-05-02 16:11:26 +0200

    Prevent NPE in status bar on alpha animation
    Change-Id: Id16f584227c35540f4a6843007da1882f4653165
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -63,6 +63,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     }
 
     public ObjectAnimator animateTransitionTo(View v, float toAlpha) {
+        if (v == null) return null;
         return ObjectAnimator.ofFloat(v, "alpha", v.getAlpha(), toAlpha);
     }
 
@@ -111,12 +112,12 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             anims.start();
             mCurrentAnimation = anims;
         } else {
-            mLeftSide.setAlpha(newAlpha);
-            mStatusIcons.setAlpha(newAlpha);
-            mSignalCluster.setAlpha(newAlpha);
-            mNetworkTraffic.setAlpha(newAlpha);
-            mBattery.setAlpha(newAlphaBC);
-            mClock.setAlpha(newAlphaBC);
+            if (mLeftSide != null) mLeftSide.setAlpha(newAlpha);
+            if (mStatusIcons != null) mStatusIcons.setAlpha(newAlpha);
+            if (mSignalCluster != null) mSignalCluster.setAlpha(newAlpha);
+            if (mNetworkTraffic != null) mNetworkTraffic.setAlpha(newAlpha);
+            if (mBattery != null) mBattery.setAlpha(newAlphaBC);
+            if (mClock != null) mClock.setAlpha(newAlphaBC);
         }
     }
 }
